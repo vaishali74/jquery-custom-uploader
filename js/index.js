4a7e43337d44dee428b6fd38ee929283b028ff79
vaishali74 <vaishaliprajapati74@gmail.com>
2023-05-31 13:07:41 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
+$(document).ready(function() {
 
+    $('#files').customUploader({
+    	extension: 'jpg,jpeg,png',
+    	text: 'Drag images here'
+    });
+
+});
+
+function getFiles() {
+    var files = $.data($('#files')[0], 'customUploader').files;
+    console.log(files);
+}
